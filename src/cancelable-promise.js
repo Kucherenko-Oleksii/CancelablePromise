c7faa33b60a99757bc6f13a4f79d8c13ac8c5812
Oleksii Kucherenko <91497964+Kucherenko-Oleksii@users.noreply.github.com>
2024-02-15 23:56:37 +0200

    Update Canelable Promise

--- src/cancelable-promise.js
+++ src/cancelable-promise.js
@@ -5,43 +5,24 @@ class CancelablePromise {
     }
 
     this._canceled = false;
-    this._promise = new Promise(async (resolve, reject) => {
-      try {
-        if (this._canceled) {
-          reject({ isCanceled: true });
-          return;
-        }
-
-        await executor(
-          (value) => {
-            if (!this._canceled) {
-              resolve(value);
-            }
-          },
-          (reason) => {
-            if (!this._canceled) {
-              reject(reason);
-            } else {
-              reject({ isCanceled: true });
-            }
+    this._promise = new Promise((resolve, reject) => {
+      executor(
+        value => {
+          if (!this._canceled) {
+            resolve(value);
           }
-        );
-      } catch (error) {
-        if (!this._canceled) {
-          reject(error);
-        } else {
-          reject({ isCanceled: true });
+        },
+        reason => {
+          if (!this._canceled) {
+            reject(reason);
+          }
         }
-      }
-    }).catch((error) => {
-      if (!this._canceled) {
-        console.log("Unhandled promise rejection:", error);
-      }
+      );
     });
   }
 
   static resolve(value) {
-    return new CancelablePromise((resolve) => resolve(value));
+    return new CancelablePromise(resolve => resolve(value));
   }
 
   static reject(reason) {
@@ -49,24 +30,11 @@ class CancelablePromise {
   }
 
   then(onFulfilled, onRejected) {
-    return new CancelablePromise(async (resolve, reject) => {
-      try {
-        const result = await this._promise;
+    const proxyFulfilled = value => (!this._canceled ? onFulfilled(value) : undefined);
+    const proxyRejected = reason => (!this._canceled ? onRejected(reason) : undefined);
 
-        if (this._canceled) {
-          reject({ isCanceled: true });
-        } else {
-          const callback = this._canceled ? onRejected : onFulfilled;
-          resolve(callback(result));
-        }
-      } catch (error) {
-        if (this._canceled) {
-          reject({ isCanceled: true });
-        } else {
-          reject(error);
-        }
-      }
-    });
+    const chainedPromise = this._promise.then(proxyFulfilled, proxyRejected);
+    return new CancelablePromise((resolve, reject) => chainedPromise.then(resolve, reject));
   }
 
   catch(onRejected) {
@@ -75,21 +43,10 @@ class CancelablePromise {
 
   cancel() {
     this._canceled = true;
-    if (typeof this._promise.cancel === "function") {
-      this._promise.cancel();
-    } else {
-      this._promise = new CancelablePromise((resolve, reject) =>
-        reject({ isCanceled: true })
-      );
-    }
   }
 
   get isCanceled() {
     return this._canceled;
-  }
-
-  set isCanceled(value) {
-    this._canceled = value;
   }
 }
 
